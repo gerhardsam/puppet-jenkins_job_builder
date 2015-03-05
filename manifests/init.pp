abc477d3c70340e05c9b7b83632d5ac011fedb4d
Sebastian Gerhards <sebastian.gerhards@aktion-mensch.de>
2015-03-05 11:57:45 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+# == Class: jenkins_job_builder
+#
+# Installs Jenkins Job Builder from OpenStack.
+#
+# === Parameters
+#
+# No special parameters.
+#
+# === Variables
+#
+# No special variables.
+#
+# === Examples
+#
+#  include jenkins_job_builder
+#
+#  class { 'jenkins_job_builder': }
+#
+# === Authors
+#
+# Sebastian Gerhards <sebastian.gerhards@aktion-mensch.de>
+#
+# === Copyright
+#
+# Copyright 2015 Sebastian Gerhards, unless otherwise noted.
+#
+class jenkins_job_builder {
+}
